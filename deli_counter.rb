a27d816e6b5129e837358084a443f32d72607aec
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-07-30 23:24:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,15 +13,35 @@ def line (queue)
   end
 end
 
+<<<<<<< HEAD
 def take_a_number(queue, name)
   queue << name
   puts "Welcome, #{name}. You are number #{queue.length} in line."
+=======
+def take_a_number (queue, name) 
+  counter = 1 
+  if queue.length == 0 
+    puts "Welcome, #{name}. Yo."
+  #else
+  #while queue.length > counter
+   # puts "Welcome, #{name}. You are number #{queue.length} in line."
+    #counter += 1
+  end
+>>>>>>> 8c504951049c0d6685ac3dbed18fb1f6966c213f
 end
 
 def now_serving  (queue) 
   if queue.length == 0 
     puts "There is nobody waiting to be served!"
   else
+<<<<<<< HEAD
     puts "Currently serving #{queue.shift}."
+=======
+    counter = 1 
+    while queue.length > counter
+      puts "Currently serving #{queue.shift}."
+      counter +=1
+    end
+>>>>>>> 8c504951049c0d6685ac3dbed18fb1f6966c213f
   end
 end
